341d6241d8b47a82c67be053609594a41504ebdd
Begench <Begench@Sirins-MacBook-Pro.local>
2025-06-25 12:46:14 +0500

    fixes -2

--- Sources/NavigationStackBackport/UIKitNavigation.swift
+++ Sources/NavigationStackBackport/UIKitNavigation.swift
@@ -1,30 +1,32 @@
 import SwiftUI
 
 struct UIKitNavigation<Root: View>: UIViewControllerRepresentable {
-	let root: Root
-	let path: NavigationPathBackport
-	@Environment(\.navigationAuthority) private var authority
-
-	func makeUIViewController(context: Context) -> UINavigationController {
-		let navigationController = UINavigationController()
-		navigationController.navigationBar.prefersLargeTitles = true
-		navigationController.navigationBar.barStyle = .default
-		navigationController.navigationBar.isTranslucent = true
-		authority.navigationController = navigationController
-		return navigationController
-	}
-
-	func updateUIViewController(_ navigationController: UINavigationController, context: Context) {
-		if !navigationController.viewControllers.isEmpty, let hostingController = navigationController.viewControllers[0] as? UIHostingController<Root> {
-			hostingController.rootView = root
-		} else {
-			let rootViewController = UIHostingController(rootView: root)
-			navigationController.viewControllers = [rootViewController]
-			prelayout(rootViewController: rootViewController, navigationController: navigationController)
-		}
-
-		authority.update(path: path)
-	}
+    let root: Root
+    let path: NavigationPathBackport
+    @Environment(\.navigationAuthority) private var authority
+    
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let navigationController = UINavigationController()
+#if os(iOS)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.barStyle = .default
+#endif
+        navigationController.navigationBar.isTranslucent = true
+        authority.navigationController = navigationController
+        return navigationController
+    }
+    
+    func updateUIViewController(_ navigationController: UINavigationController, context: Context) {
+        if !navigationController.viewControllers.isEmpty, let hostingController = navigationController.viewControllers[0] as? UIHostingController<Root> {
+            hostingController.rootView = root
+        } else {
+            let rootViewController = UIHostingController(rootView: root)
+            navigationController.viewControllers = [rootViewController]
+            prelayout(rootViewController: rootViewController, navigationController: navigationController)
+        }
+        
+        authority.update(path: path)
+    }
 }
 
 private extension UIKitNavigation {
